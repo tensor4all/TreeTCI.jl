2e59ef2d094c2e440991617a9410a6e4e49e5311
Documenter.jl <documenter@juliadocs.github.io>
2024-11-18 18:56:30 +0000

    build based on d83f103

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TreeTCI","category":"page"},{"location":"#TreeTCI","page":"Home","title":"TreeTCI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is the documentation for TreeTCI.","category":"page"}]
+}
